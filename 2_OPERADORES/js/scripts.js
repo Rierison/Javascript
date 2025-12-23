8d5df263cc869dcc423a62502b797a50ae413e30
Rierison <rierisonpinheiro@gmail.com>
2025-12-23 01:31:46 -0300

    Variáveis

--- 2_OPERADORES/js/scripts.js
+++ 2_OPERADORES/js/scripts.js
@@ -28,9 +28,90 @@
 // console.log(typeof "Um texto");
 // console.log(typeof 'mais um texto');
 
-// 5 - Símbolos especiais com strings
-console.log("Testando a \nquebra de linha");
-console.log("Espaçamento \t de tab");
+// // 5 - Símbolos especiais com strings
+// console.log("Testando a \nquebra de linha");
+// console.log("Espaçamento \t de tab");
 
-// 6 - Concatenação
-console.log("Oi," + " tudo" + " bem?")
+// // 6 - Concatenação
+// console.log("Oi," + " tudo" + " bem?");
+
+// console.log(`Testando ` + `com ` + `crase!`);
+
+// // 7 - Templates Strings
+// console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+// console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
+
+
+// // 8 - Booleans
+// console.log(true);
+// console.log(false);
+// console.log(typeof true);
+// console.log(typeof false);
+// console.log(5 > 20);
+// console.log(30 > 10);
+
+// // 9 - Comparações
+// console.log(5 <= 5);
+
+// console.log(5 < 5);
+
+// console.log(10 == 10);
+
+// console.log(10 == 9);
+
+// console.log(10 != 9);
+
+// // 10 - Valor Idêntico
+// console.log(9 == "9");
+
+// console.log(9 === "9");
+
+// console.log(9 != "9");
+
+// console.log(9 !== "9");
+
+// 11 - Operadores Lógicos  
+// console.log(true && true);
+
+// console.log(true && false);
+
+// console.log(5 > 2 && 2 < 10);   // - && (AND)
+
+// console.log(5 > 2 && "Matheus" === 1);  
+
+// console.log(5 > 2 || "Matheus" === 1);   // - || (OR)
+
+// console.log(false || false);
+
+// console.log(!true);   // - ! (NOT)
+
+// 12 - Empty Values
+// console.log(null);
+// console.log(typeof null, typeof undefined);
+
+// console.log(null === undefined);
+
+// console.log(null == undefined);
+
+// console.log(null == false);
+
+// console.log(undefined == false);
+
+// 13 - Mudanças de tipos 
+
+// console.log(5 * null);
+
+// console.log("Teste" * "opa");
+
+// console.log("10" + 1);
+
+// console.log("10" - 1);
+
+
+
+
+
+
+
+
---
+++ 3_ESTRUTURAS/js/scripts.js
@@ -0,0 +1,39 @@
+// 1 - Variáveis
+let nome = "Rierison";
+
+console.log(nome);
+
+nome = "Rierison Pinheiro";
+
+console.log(nome);
+
+const idade = 31;
+
+console.log(idade);
+
+console.log(typeof nome);
+
+console.log(typeof idade);  
+
+// 2 - Mais sobre Variáveis
+let a = 10,
+b = 20,
+c = 30;
+
+console.log(a, b, c);
+
+const nomecompleto = "Rierison Pinheiro Matos";
+
+const nomeCompleto = "Rierison Pinheiro Matos";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);  
+
+// Simbolos permitidos para criação de variáveis
+let $teste = "ok"; // cifrão
+
+let _teste = "ok"; // underline
+
+console.log($teste, _teste);
+
