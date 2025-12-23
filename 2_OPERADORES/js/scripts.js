a812ceb3d118eebf9f447625c1583c1efd449b05
Rierison <rierisonpinheiro@gmail.com>
2025-12-23 00:21:13 -0300

    Concatenação

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,36 @@
+// // 1 - Number
+
+// console.log(typeof 2);
+// console.log(typeof 5.14);
+// console.log(typeof -127);
+
+// // 2 - Operações Aritméticas
+// console.log(2 + 4);
+// console.log(10 - 5);
+// console.log(3 * 7);
+// console.log(20 / 5);
+
+
+// console.log(5 + (4 * 2));
+
+// // 3 - Special Numbers
+
+// console.log(typeof Infinity);
+// console.log(typeof -Infinity);
+// console.log(12 * "asd");
+// console.log(typeof NaN);
+
+// //  4 - Tipos de Strings
+// console.log("Um texto");
+// console.log('mais um texto');
+
+
+// console.log(typeof "Um texto");
+// console.log(typeof 'mais um texto');
+
+// 5 - Símbolos especiais com strings
+console.log("Testando a \nquebra de linha");
+console.log("Espaçamento \t de tab");
+
+// 6 - Concatenação
+console.log("Oi," + " tudo" + " bem?")
