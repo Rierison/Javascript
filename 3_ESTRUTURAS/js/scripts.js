27050ad7c076ce0b132775901848fb94c051c236
Rierison <rierisonpinheiro@gmail.com>
2025-12-23 18:59:31 -0300

    Math

--- 3_ESTRUTURAS/js/scripts.js
+++ 3_ESTRUTURAS/js/scripts.js
@@ -1,39 +1,64 @@
-// 1 - Variáveis
-let nome = "Rierison";
+// // 1 - Variáveis
+// let nome = "Rierison";
 
-console.log(nome);
+// console.log(nome);
 
-nome = "Rierison Pinheiro";
+// nome = "Rierison Pinheiro";
 
-console.log(nome);
+// console.log(nome);
 
-const idade = 31;
+// const idade = 31;
 
-console.log(idade);
+// console.log(idade);
 
-console.log(typeof nome);
+// console.log(typeof nome);
 
-console.log(typeof idade);  
+// console.log(typeof idade);  
 
-// 2 - Mais sobre Variáveis
-let a = 10,
-b = 20,
-c = 30;
+// // 2 - Mais sobre Variáveis
+// let a = 10,
+// b = 20,
+// c = 30;
 
-console.log(a, b, c);
+// console.log(a, b, c);
 
-const nomecompleto = "Rierison Pinheiro Matos";
+// const nomecompleto = "Rierison Pinheiro Matos";
 
-const nomeCompleto = "Rierison Pinheiro Matos";
+// const nomeCompleto = "Rierison Pinheiro Matos";
 
-console.log(nomecompleto);
+// console.log(nomecompleto);
 
-console.log(nomeCompleto);  
+// console.log(nomeCompleto);  
 
-// Simbolos permitidos para criação de variáveis
-let $teste = "ok"; // cifrão
+// // Simbolos permitidos para criação de variáveis
+// let $teste = "ok"; // cifrão
 
-let _teste = "ok"; // underline
+// let _teste = "ok"; // underline
 
-console.log($teste, _teste);
+// console.log($teste, _teste);
+
+// prompt
+// const age = prompt("Digite a sua idade?");
+
+// console.log(`Você tem ${age} anos`);
+
+// 4 - alert
+// alert("Testando");
+
+// const z = 10;
+
+// alert(`O número é ${z}`);
+
+// 5 - Math // Biblioteca de funções para fins matemáticos
+console.log(Math.max(5, 2, 1, 10)); // maior número
+
+console.log(Math.floor(5.14)); // arredondamento de números
+
+console.log(Math.ceil(5.14)); // arredondamento para cima
+
+console.log(Math.round(5.5)); // arredondamento para o mais próximo
+
+
+
+
 
