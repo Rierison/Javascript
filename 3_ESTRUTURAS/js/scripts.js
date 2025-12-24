a9d1187c867795a83943dadca404c97685159975
Rierison <rierisonpinheiro@gmail.com>
2025-12-23 21:01:52 -0300

    if

--- 3_ESTRUTURAS/js/scripts.js
+++ 3_ESTRUTURAS/js/scripts.js
@@ -49,16 +49,56 @@
 
 // alert(`O número é ${z}`);
 
+
+
 // 5 - Math // Biblioteca de funções para fins matemáticos
-console.log(Math.max(5, 2, 1, 10)); // maior número
+// console.log(Math.max(5, 2, 1, 10)); // maior número
 
-console.log(Math.floor(5.14)); // arredondamento de números
+// console.log(Math.floor(5.14)); // arredondamento de números
 
-console.log(Math.ceil(5.14)); // arredondamento para cima
+// console.log(Math.ceil(5.14)); // arredondamento para cima
 
-console.log(Math.round(5.5)); // arredondamento para o mais próximo
-
-
+// console.log(Math.round(5.5)); // arredondamento para o mais próximo
 
 
+// 6 - Console
+// console.log("Teste!"); // Mensagem de log
 
+// console.error("Erro!"); // Mensagem de erro
+
+// console.warn("Aviso!"); // Mensagem de aviso
+
+
+
+// 7 - if
+const m = 10;
+
+if (m > 5) {
+    console.log("M é maior que 5");
+}
+
+const user = "João";
+
+if(user === "João") {
+    console.log("Olá, João!");
+
+}
+
+if(user === "Maria") {
+    console.log("Olá, Maria!");
+}
+
+console.log(user === "João", user === "Maria"); // true
+
+
+// const idade = 18;
+
+// if (idade >= 18) {
+//     console.log("Maior de idade");
+// }
+
+
+// ...else - // tomada de decisão
+
+
+// Estrutura de Controle: 
